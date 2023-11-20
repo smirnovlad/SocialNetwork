b865b6e557e09ecbbe0eabe5486194b837c6593b
Smirnov Vlad <smirnov.vladislav@phystech.edu>
2023-11-20 15:59:32 +0300

    photo handling is added

--- backend/messenger/admin.py
+++ backend/messenger/admin.py
@@ -3,8 +3,8 @@ from .models import User
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['login', 'password', 'firstName', 'lastName', 'bornAt', 'homeTown']
-    list_editable = ['password']
+    list_display = ['login', 'password', 'firstName', 'lastName', 'bornAt', 'homeTown', 'avatar']
+    list_editable = ['password', 'avatar']
     list_display_links = ['login']
 
 admin.site.register(User, UserAdmin)
