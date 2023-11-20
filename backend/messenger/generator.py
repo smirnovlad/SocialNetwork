3134572273c61c5af40f256b033e6a5af0f5d815
Smirnov Vlad <smirnov.vladislav@phystech.edu>
2023-11-20 15:28:28 +0300

    generator is fixed

--- backend/messenger/generator.py
+++ backend/messenger/generator.py
@@ -6,13 +6,13 @@ from django.db import connection
 from random_username.generate import generate_username
 
 def generate():
-    user1 = User(login=generate_username(), password="qwerty123", firstName="Vlad", lastName="Smirnov",
+    user1 = User(login=generate_username()[0], password="qwerty123", firstName="Vlad", lastName="Smirnov",
                  bornAt=datetime.date(2000, 5, 9), homeTown="Moscow")
     print(f"user id: {user1.id}, {user1}")
     user1.save()
     print(f"user id: {user1.id}")
 
-    user2 = User(login=generate_username(), password="password", firstName="Alexander", lastName="Ivanov",
+    user2 = User(login=generate_username()[0], password="password", firstName="Alexander", lastName="Ivanov",
                  bornAt=datetime.date(2001, 3, 8), homeTown="Moscow")
     print(f"user id: {user2.id}, {user2}")
     user2.save()
