5d6f922b9329cc8faeb639f70795fcfc6dee06ab
Smirnov Vlad <smirnov.vladislav@phystech.edu>
2023-11-21 15:06:11 +0300

    Example of ModelSerializer's usage is added

--- backend/messenger/serializers.py
+++ backend/messenger/serializers.py
@@ -6,26 +6,10 @@ from rest_framework.renderers import JSONRenderer
 
 from .models import User
 
-class UserSerializer(serializers.Serializer):
-    login = serializers.CharField(max_length=30)
-    password = serializers.CharField(max_length=30)
-    firstName = serializers.CharField(max_length=30)
-    lastName = serializers.CharField(max_length=30)
-    bornAt = serializers.DateField()
-    homeTown = serializers.CharField(max_length=30)
-
-    def create(self, validated_data):
-        return User.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.login = validated_data.get('login', instance.login)
-        instance.password = validated_data.get('password', instance.password)
-        instance.firstName = validated_data.get('firstName', instance.firstName)
-        instance.lastName = validated_data.get('lastName', instance.lastName)
-        instance.bornAt = validated_data.get('bornAt', instance.bornAt)
-        instance.homeTown = validated_data.get('homeTown', instance.homeTown)
-        instance.save()
-        return instance
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['login', 'password', 'firstName', 'lastName', 'bornAt', 'homeTown']
 
 class FriendsSerializer(serializers.Serializer):
     firstUser = serializers.IntegerField()
