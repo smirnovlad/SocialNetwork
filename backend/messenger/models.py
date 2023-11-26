a640a6e14cf9fb2ae35ac46567cc0bda5b2a5187
Smirnov Vlad <smirnov.vladislav@phystech.edu>
2023-11-26 23:53:17 +0300

    password is already exist in AbstractBaseUser

--- backend/messenger/models.py
+++ backend/messenger/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    password = models.CharField(max_length=50)
     bornAt = models.DateField()
     homeTown = models.CharField(max_length=30)
     avatar = models.ImageField(upload_to="uploads/avatars/", blank=True)
