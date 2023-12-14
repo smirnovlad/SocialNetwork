d59bfe9229f3d601f52cae593a6490ec36ea28c8
Smirnov Vlad <smirnov.vladislav@phystech.edu>
2023-12-14 19:44:10 +0300

    permission classes are added

--- backend/messenger/views.py
+++ backend/messenger/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 from .generator import generate
 from rest_framework import generics, mixins
 from .models import User, Message, Feedback, Chat
@@ -22,18 +24,22 @@ class CustomAPIViews(mixins.ListModelMixin,
 class UserAPIViews(CustomAPIViews):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class MessageAPIViews(CustomAPIViews):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class ChatAPIViews(CustomAPIViews):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class FeedbackAPIViews(CustomAPIViews):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
